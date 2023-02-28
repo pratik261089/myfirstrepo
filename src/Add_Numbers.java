c42a6ae5390d10a85507482ad86676a573194038
Pratik Bhosle <pratikbhosle.21@gmail.com>
2023-02-28 15:09:51 +0530

    My first commit

---
+++ src/Add_Numbers.java
@@ -0,0 +1,18 @@
+
+public class Add_Numbers {
+	
+	public static void addnumbers(int a, int b ) {
+		
+		int sum = a+b;
+		
+		System.out.println(sum);
+		
+	}
+
+	public static void main(String[] args) {
+		
+	addnumbers(5, 10);
+
+	}
+
+}
