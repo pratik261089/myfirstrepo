7bdc27c052fdf92f3a87bcf59ce7a2a001e4907a
Pratik Bhosle <pratikbhosle.21@gmail.com>
2023-02-28 16:21:42 +0530

    Second commit from my side

---
+++ src/Diff_Numbers.java
@@ -0,0 +1,18 @@
+
+public class Diff_Numbers {
+	
+public static void diffnumbers(int a, int b ) {
+		
+		int sum = a-b;
+		
+		System.out.println(sum);
+		
+	}
+
+	public static void main(String[] args) {
+		
+		diffnumbers(15, 10);
+		
+	}
+
+}
